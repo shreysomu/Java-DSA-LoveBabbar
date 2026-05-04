664e793560503da65b1b3906aeb9e225e0727fca
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2026-05-04 19:00:40 +0530

    Completed revising data types
    and solved some basic problem

---
+++ lecture6/DataTypes.java
@@ -0,0 +1,25 @@
+package dsa.com.lecture6;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        byte num1 = 127;
+        short num2 = 5676;
+
+        int num3 = 450090;
+        long num4 = 456769039;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+
+        //floating value.....
+
+        float num5 = 3.245666f;
+        double num6 = 6.4679790302011111;
+
+        System.out.println(num5);
+        System.out.println(num6);
+    }
+}
