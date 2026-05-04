ca06521616167cd477a35d1c0f24efd96d7697d0
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2026-05-04 19:01:34 +0530

    Practiced Operator

---
+++ lecture7/Operators.java
@@ -0,0 +1,9 @@
+package dsa.com.lecture7;
+
+public class Operators {
+    public static void main(String[] args) {
+        int num = 24;
+        int num2 = 34;
+        System.out.println(num + num2);
+    }
+}
