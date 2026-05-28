eaa084debde0aa748582df57a075df9994edb5c9
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2026-05-28 14:55:46 +0530

    Add files via upload

---
+++ lecture30_ArrayPart4/FindPivotIndex.java
@@ -0,0 +1,32 @@
+package dsa.com.lecture30_ArrayPart4;
+
+//Pivot index -----> which is having same of left side's element == sum of right side's element
+public class FindPivotIndex {
+
+    static int pivotIndex(int[] arr){
+       int n = arr.length;
+       int []leftSum = new int[n];
+       int[] rightSum = new int[n];
+
+       leftSum[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            leftSum[i] = leftSum[i-1] + arr[i];
+        }
+
+        rightSum[n-1] = arr[n-1];
+        for(int i = n-2;i>=0;i--){
+            rightSum[i] = rightSum[i+1] + arr[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(leftSum[i] == rightSum[i])
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,7,3,6,5,6};
+        System.out.println("Pivot index is : " + pivotIndex(arr));
+    }
+}
